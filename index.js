d3e67b057a10fb8583987e8e5706ac60073f3e1c
Hoho-san <javierjojo24@gmail.com>
2022-12-05 20:18:54 +0800

    Hello

---
+++ index.js
@@ -0,0 +1,15 @@
+const noBtn = document.getElementById("Hinde");
+const yesBtn = document.getElementById("Oo");
+const ques = document.getElementById("question");
+let btn = document.getElementById("btn");
+let position;
+
+noBtn.addEventListener("mouseover", () => {
+    let rand = Math.floor(Math.random() * (500 - 100) + 1);
+    let rand2 = Math.floor(Math.random() * (-300 - 100) + 1);
+    noBtn.style.transform = "translate(" + rand + "px," + rand2 + "px)";
+});
+
+yesBtn.addEventListener("click", () => {
+    ques.innerHTML = "Ikaw Haaaaa HAHAHAHAHA <3";
+});
