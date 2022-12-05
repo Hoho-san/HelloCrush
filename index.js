fe781986525bd2de69eb17136b86e5b8651be9cf
Hoho-san <javierjojo24@gmail.com>
2022-12-05 20:52:36 +0800

    centering the div and some value adjustment

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ let btn = document.getElementById("btn");
 let position;
 
 noBtn.addEventListener("mouseover", () => {
-    let rand = Math.floor(Math.random() * (500 - 100) + 1);
-    let rand2 = Math.floor(Math.random() * (-300 - 100) + 1);
+    let rand = Math.floor(Math.random() * 250 + 1);
+    let rand2 = Math.floor(Math.random() * 250 + 2);
+    noBtn.style.transform = "translate(" + rand + "px," + rand2 + "px)";
+});
+noBtn.addEventListener("click", () => {
+    let rand = Math.floor(Math.random() * (100 - 50) + 1);
+    let rand2 = Math.floor(Math.random() * (-100 - 50) + 1);
     noBtn.style.transform = "translate(" + rand + "px," + rand2 + "px)";
 });
 
