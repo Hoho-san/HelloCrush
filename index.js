115aba388edda9e1f0c10f53bb1ebaac6a7d6837
Hoho-san <javierjojo24@gmail.com>
2023-01-13 17:38:30 +0800

    cleaning

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let btn = document.getElementById("btn");
 let position;
 
 noBtn.addEventListener("mouseover", () => {
-    let rand = Math.floor(Math.random() * 250 + 1);
-    let rand2 = Math.floor(Math.random() * 250 + 2);
+    let rand = Math.floor(Math.random() * 300 + 1);
+    let rand2 = Math.floor(Math.random() * 300 + 1);
     noBtn.style.transform = "translate(" + rand + "px," + rand2 + "px)";
 });
 noBtn.addEventListener("click", () => {
-    let rand = Math.floor(Math.random() * 200 + 1);
-    let rand2 = Math.floor(Math.random() * 200 + 1);
+    let rand = Math.floor(Math.random() * 300 + 1);
+    let rand2 = Math.floor(Math.random() * 300 + 1);
     noBtn.style.transform = "translate(" + rand + "px," + rand2 + "px)";
 });
 
